899719062617d46bfbc5bd9e82faae1ece2acd08
Pedro Carvalho <pedropaulohouston@gmail.com>
2021-10-15 10:53:37 -0300

    First Commit

---
+++ primeiro.go
@@ -0,0 +1,14 @@
+// determinar o package serva para que toda variavel que for definida
+// vai ser visivel em todos os arquivos que estao nesse pacote (arquivos no mesmo dir)
+// serve tambem para funcoes, constates
+// Para ser executavel tem que estar no main
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Olá mundo!!")
+}
