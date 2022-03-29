fceac8a07f4dd3927b4b829e4800e3077760fa3f
Pedro Carvalho <pedropaulohouston@gmail.com>
2022-03-29 20:58:58 -0300

    Fundamentos da Linguagem

--- 2 - Variables/variaveis.go
+++ 2 - Variables/variaveis.go
@@ -20,4 +20,11 @@ func main() {
 
 	variavel5, variavel6 := "Variavel 5", "Variavel 6"
 	fmt.Println(variavel5, variavel6)
+
+	const constante1 string = "Constante 1"
+	fmt.Println(constante1)
+
+	// realizar troca de valores
+	variavel5, variavel6 = variavel6, variavel5
+	fmt.Println(variavel5, variavel6)
 }
